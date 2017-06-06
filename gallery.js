9e465c8d25af73afdfaf33c87b43ac92bb28859a
totonaq <mr.kymman@gmail.com>
2017-06-06 23:11:48 +0300

    fixed drag function

--- gallery.js
+++ gallery.js
@@ -25,7 +25,6 @@
 	//default settings
 
 	let defaults = {
-		//defaultImg: 2,
 		height: 600,
 		thumbHeight: 150,
 		items: 3,
@@ -63,63 +62,47 @@
 			
 		},
 		createElements: function() {
-			container.prepend('<div class="large-image"></div><div class="thumbnails"></div>');
-
+			container.prepend('<div class="large-image">' + 
+				'</div><div class="thumbnails"></div>');
 			largeImg = container.find('.large-image')
-
 			container.css({
 				'width':'100%',
 				'overflow-x':'hidden',
 				'outline':'none'
 			});
-
 			allLargeImg.push(container.find('.large-image'));
 
-
-			container.append('<div class="controls"><div class="prev-button"></div><div class="next-button"></div></div>');
+			container.append('<div class="controls">' + 
+				'<div class="prev-button"></div>' + 
+				'<div class="next-button"></div></div>');
 
 			prevBtn = container.find('.prev-button');
-
 			prevBtn.addClass('inactive');
-
 			allPrev.push(prevBtn);
-
 			nextBtn = container.find('.next-button');
-
 			allNext.push(nextBtn);
 
-
 			currentThumbs = container.find('.thumbnails');
-
 			currentThumbs.wrap('<div class="thmb-wrap"></div>')
-
 			currentThumbs.append(container.find('img'));
-
-			currentThumbs.find('img').wrap('<div class="gallery-item"></div>');
-
+			currentThumbs.find('img')
+				.wrap('<div class="gallery-item"></div>');
 			allThumbs.push(currentThumbs);
 
-
 			currentGalItems = container.find('.gallery-item');
-
 			currentGalItems.eq(0).addClass('active');
-
-
 			allGalItems.push(currentGalItems);
-
 			galItemLength.push(currentGalItems.length);
-
-
 			currentGalItems.each(function(elem) {
-				$(this).css('background-image', 'url('+ $(this).find('img').attr('src') +')');
-				$(this).find('img').remove()
+				$(this).css('background-image', 
+					'url('+ $(this).find('img').attr('src') +')');
+				$(this).find('img').remove();
 			});
 
-
-			let currentImage = currentGalItems.eq(0).css('background-image');
+			let currentImage = currentGalItems.eq(0)
+				.css('background-image');
 
 			largeImg.css('background-image', currentImage);
-
 
 			thumbWrap = $('.thmb-wrap');
 
@@ -141,8 +124,6 @@
 			thumbWrap.css('overflow-x', 'hidden');
 
 			currentThumbs.css({
-				'cursor': 'grab' || '-o-grab' || '-moz-grab' ||
-				 '-webkit-grab',
 				'display': 'flex' || '-webkit-flex' || 
 				'-moz-flex' || '-o-flex',
 				'flex-flow': 'row wrap'
@@ -170,8 +151,12 @@
 			
 				(function resp(item) {
 					if (item <= last - 1) {
-						if (allLargeImg[i].width() >= keys[item] && allLargeImg[i].width() < keys[item + 1]) {
-						options = $.extend({}, respPropArr[i], propArr[i][keys[item + 1]]);
+						if (allLargeImg[i].width() >= 
+							keys[item] && allLargeImg[i].width() < 
+							keys[item + 1]) {
+						options = $.extend({}, 
+							respPropArr[i], 
+							propArr[i][keys[item + 1]]);
 						} else {
 							resp(item + 1);
 						}
@@ -197,7 +182,8 @@
 
 		setThumbnailsWrapWidth: function() {
 			for (let i = 0; i < propArr.length; i++) {
-				allThumbs[i].width(allLargeImg[i].width() / propArr[i].items * galItemLength[i]);
+				allThumbs[i].width(allLargeImg[i].width() / 
+					propArr[i].items * galItemLength[i]);
 			}
 			
 		},
@@ -205,7 +191,8 @@
 		setThumbnailsDimensions: function() {
 			for (let i = 0; i < propArr.length; i++) {
 				allGalItems[i].css({
-					'width':allLargeImg[i].width() / propArr[i].items,
+					'width':allLargeImg[i].width() / 
+					propArr[i].items,
 					'height':propArr[i].thumbHeight
 				});
 			}
@@ -216,24 +203,26 @@
 
 				let clickedImg = $(this).css('background-image');
 
-				$(this).parent().parent().prev().css('background-image', clickedImg);
-
+				$(this).parent().parent().prev()
+					.css('background-image', clickedImg);
 				$(this).parent().children().removeClass('active');
-				
 				$(this).addClass("active");
-
-
-
 
 				let items = $(this).parent().children();
 
-				$(this).parent().parent().next().children().removeClass('inactive');
-
+				$(this).parent().parent().next()
+					.children().removeClass('inactive');
+				
 				if (items.index($(this)) === 0) {
-					$(this).parent().parent().next().find('.prev-button').addClass('inactive');
+					$(this).parent().parent().next()
+						.find('.prev-button')
+						.addClass('inactive');
 				}
-				if (items.index($(this)) === items.length - 1) {
-					$(this).parent().parent().next().find('.next-button').addClass('inactive');
+				if (items.index($(this)) === 
+					items.length - 1) {
+					$(this).parent().parent().next()
+						.find('.next-button')
+						.addClass('inactive');
 				}
 
 			});
@@ -244,9 +233,11 @@
 			
 			prevBtn.click(function() {
 				
-				let active = $(this).parent().prev().find('.active').prev();
+				let active = $(this).parent().prev()
+					.find('.active').prev();
 
-				let allItems = $(this).parent().prev().find('.gallery-item');
+				let allItems = $(this).parent().prev()
+					.find('.gallery-item');
 
 				let number = $(allItems).index(active);
 
@@ -260,9 +251,10 @@
 					$(this).parent().prev().scrollLeft(0);
 				}
 				
-				methods.setPositionOfItem.call(this, active, number, allItems);
-				
-				methods.makeInactiveBtn.call(this, number, allItems);
+				methods.setPositionOfItem.call(this, 
+					active, number, allItems);
+				methods.makeInactiveBtn.call(this, 
+					number, allItems);
 				
 			});
 			
@@ -272,45 +264,62 @@
 			
 			nextBtn.click(function() {
 
-				let active = $(this).parent().prev().find('.active').next();
+				let active = $(this).parent().prev()
+					.find('.active').next();
 
-				let allItems = $(this).parent().prev().find('.gallery-item');
+				let allItems = $(this).parent().prev()
+					.find('.gallery-item');
 
 				let number = $(allItems).index(active);
 
 				if (number < allItems.length) {
-					$(this).parent().prev().prev().css('background-image', active.css('background-image'));
+
+					$(this).parent().prev().prev()
+						.css('background-image', 
+							active.css('background-image'));
 					active.addClass('active');
 					active.prev().removeClass('active');
+
 				} else {
-					$(this).parent().prev().scrollLeft(allItems.length * allItems.width());
+
+					$(this).parent().prev()
+						.scrollLeft(allItems.length * 
+							allItems.width());
 				}
 			
-				methods.setPositionOfItem.call(this, active, number, allItems);
-
-				methods.makeInactiveBtn.call(this, number, allItems);
+				methods.setPositionOfItem.call(this, 
+					active, number, allItems);
+				methods.makeInactiveBtn.call(this, 
+					number, allItems);
 				
 			});
 
 		},
 
 		setPositionOfItem: function(active, number, allItems) {
-			if (active.position() != undefined && active.position().left < -5) {
+			if (active.position() != undefined 
+				&& active.position().left < -5) {
 
-				$(this).parent().prev().scrollLeft((number + 1)* allItems.width() - $(this).parent().parent().width());
-
+					$(this).parent().prev()
+						.scrollLeft((number + 1)* allItems.width() - 
+						$(this).parent().parent().width());
 			}
 
-			if (active.position() != undefined && active.position().left >= $(this).parent().prev().width() - 5) {
+			if (active.position() != undefined 
+				&& active.position().left >= 
+				$(this).parent().prev().width() - 5) {
 
-				$(this).parent().prev().scrollLeft((number) * allItems.width())
+					$(this).parent().prev().scrollLeft((number) * 
+						allItems.width())
 
 			}
 		},
 
 		makeInactiveBtn: function(number, allItems) {
 		
-			$(this).parent().children().removeClass('inactive');
+			$(this).parent().children()
+				.removeClass('inactive');
+
 			if (number > allItems.length - 2 || number < 0) {
 				$(this).addClass('inactive');
 			} else if (number < 1) {
@@ -326,26 +335,35 @@
 			
 		},
 	
-		hzScroll: function() {
+		hzScroll: function(el) {
+
 			let curDown = false,
-			curXPos = 0;
+				lastClientX,
+				lastClientY,
+				newScrollX;
 
-			thumbWrap.on('mousemove', function(m) {
-				if (curDown) {
-					let left = $(this).scrollLeft() + (curXPos - m.pageX)/10
-					$(this).scrollLeft(left);
-				}
+			el = thumbWrap;
 
-			});
-
-			thumbWrap.on('mousedown', function(m) {
+			el.on('mousedown', function(e) {
 				curDown = true;
-				curXPos = m.pageX;
+				lastClientX = e.clientX;
+                lastClientY = e.clientY;
+                e.preventDefault();
 			});
 
-			thumbWrap.on('mouseup', function() {
+			el.on('mouseup', function() {
 				curDown = false;
 			});
+
+			el.on('mousemove', function(e) {
+				if (curDown) {
+					let start = $(this).scrollLeft();
+					newScrollX = (- lastClientX + 
+						(lastClientX = e.clientX));
+					$(this).scrollLeft(start - newScrollX) 
+				}
+			});
+
 		},
 
 		setCustomControls: function() {
